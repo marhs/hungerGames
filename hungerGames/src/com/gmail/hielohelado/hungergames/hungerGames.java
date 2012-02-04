56a12bb286c5d735c7026bc33f51b5f11f922e9b
Marco Herrero <hielo.helado@gmail.com>
2012-02-04 12:49:59 +0100

    Añadido un comando de prueba
    El comando no hace nada

--- hungerGames/src/com/gmail/hielohelado/hungergames/hungerGames.java
+++ hungerGames/src/com/gmail/hielohelado/hungergames/hungerGames.java
@@ -1,6 +1,25 @@
 package com.gmail.hielohelado.hungergames;
 
-public class hungerGames {
+import java.util.logging.Logger;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class hungerGames extends JavaPlugin{
+	
+	Logger log = Logger.getLogger("Minecraft");
+	
+    public void onEnable() { 
+    	log.info("Hunger Games ready!");
+    }
+     
+    public void onDisable() { 
+    	log.info("Hunger Games has been disabled");
+
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+    	return false;
+    }
 }
