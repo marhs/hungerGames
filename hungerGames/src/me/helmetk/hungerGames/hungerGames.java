cd755685e9e92f9a3d96e9e6cadefca4c0242aa5
XpauM <july491@gmail.com>
2012-02-10 00:04:15 +0100

    Modificado el sistema de limpiado del spawn

--- hungerGames/src/me/helmetk/hungerGames/hungerGames.java
+++ hungerGames/src/me/helmetk/hungerGames/hungerGames.java
@@ -201,10 +201,18 @@ public class hungerGames extends JavaPlugin{
     						int spawnz = source.getZ();
     						int spawny = mundo1.getHighestBlockYAt(spawnx, spawnz);
     						
+    						//Limpiado de Inicio
     						ChunkInicio(source, spawny);
-    						ChunkInicio(mundo1.getChunkAt(spawnx+1, spawnz), spawny);
     						ChunkInicio(mundo1.getChunkAt(spawnx, spawnz+1), spawny);
+    						ChunkInicio(mundo1.getChunkAt(spawnx, spawnz-1), spawny);
+    						
+    						ChunkInicio(mundo1.getChunkAt(spawnx+1, spawnz), spawny);
     						ChunkInicio(mundo1.getChunkAt(spawnx+1, spawnz+1), spawny);
+    						ChunkInicio(mundo1.getChunkAt(spawnx+1, spawnz-1), spawny);
+    						
+    						ChunkInicio(mundo1.getChunkAt(spawnx-1, spawnz), spawny);
+    						ChunkInicio(mundo1.getChunkAt(spawnx-1, spawnz+1), spawny);
+    						ChunkInicio(mundo1.getChunkAt(spawnx-1, spawnz-1), spawny);
     						
     					}
     					
@@ -232,12 +240,104 @@ public class hungerGames extends JavaPlugin{
     
     private void ChunkInicio(Chunk source,int spawny){
     	for(int x1=0;x1<16;x1++){
-			for(int z1=0;z1<16;z1++){
-				//result[(x1 * 16 + z1) * 128 + spawny]=(byte)Material.BEDROCK.getId();
-				Block bloque=source.getBlock(x1, spawny, z1);
-				bloque.setType(Material.BEDROCK);
-				for(int y=spawny+1;y<128;y++)source.getBlock(x1, y,z1).setType(Material.AIR);
-			}
+			limpiarChunkAtX(source, spawny, x1);
+		}
+    	//Chunk 1
+    	Chunk c1=source.getWorld().getChunkAt(source.getX(), source.getZ()+1);
+    	limpiarChunkNorte(c1, spawny);
+    	//Chunk 2
+    	Chunk c2=source.getWorld().getChunkAt(source.getX()+1, source.getZ());
+    	limpiarChunkEste(c2, spawny);
+    	//Chunk 3
+    	Chunk c3=source.getWorld().getChunkAt(source.getX(), source.getZ()-1);
+    	limpiarChunkSur(c3, spawny);
+    	//Chunk 4
+    	Chunk c4=source.getWorld().getChunkAt(source.getX()-1, source.getZ());
+    	limpiarChunkOeste(c4, spawny);
+    	
+    }
+    
+    private void limpiarChunkEste(Chunk source,int spawny){
+    	//int y=source.getWorld().getHighestBlockYAt(source.getBlock(15, spawny, 7).getLocation());
+    	//Limpiar en forma de escalera
+    	for(int x=0;x<16;x++){
+    		int pase;boolean needed=false;
+    		for(int p=0;p<16;p++){
+    			pase=source.getWorld().getHighestBlockYAt(source.getBlock(x, spawny, p).getLocation())-spawny;
+    			if(pase > 2){
+    				needed=true;break;
+    			}
+    		}
+    		if(needed){
+    			limpiarChunkAtX(source, spawny+1, x);
+    		}
+    		spawny++;
+    	}
+    }
+    
+    private void limpiarChunkNorte(Chunk source,int spawny){
+    	for(int z=0;z<16;z++){
+    		int pase;boolean needed=false;
+    		for(int p=0;p<16;p++){
+    			pase=source.getWorld().getHighestBlockYAt(source.getBlock(p, spawny, z).getLocation())-spawny;
+    			if(pase > 2){
+    				needed=true;break;
+    			}
+    		}
+    		if(needed){
+    			limpiarChunkAtZ(source, spawny+1, z);
+    		}
+    		spawny++;
+    	}
+    }
+
+    private void limpiarChunkOeste(Chunk source,int spawny){
+    	for(int x=15;x>=0;x--){
+    		int pase;boolean needed=false;
+    		for(int p=0;p<16;p++){
+    			pase=source.getWorld().getHighestBlockYAt(source.getBlock(x, spawny, p).getLocation())-spawny;
+    			if(pase > 2){
+    				needed=true;break;
+    			}
+    		}
+    		if(needed){
+    			limpiarChunkAtX(source, spawny+1, x);
+    		}
+    		spawny++;
+    	}
+    }
+
+    private void limpiarChunkSur(Chunk source,int spawny){
+    	for(int z=15;z>=0;z--){
+    		int pase;boolean needed=false;
+    		for(int p=0;p<16;p++){
+    			pase=source.getWorld().getHighestBlockYAt(source.getBlock(p, spawny, z).getLocation())-spawny;
+    			if(pase > 2){
+    				needed=true;break;
+    			}
+    		}
+    		if(needed){
+    				limpiarChunkAtZ(source, spawny+1, z);
+    			}
+    		spawny++;
+    	}
+    }
+
+    private void limpiarChunkAtX(Chunk source,int spawny,int X){
+    	for(int z1=0;z1<16;z1++){
+			//result[(x1 * 16 + z1) * 128 + spawny]=(byte)Material.BEDROCK.getId();
+			Block bloque=source.getBlock(X, spawny, z1);
+			bloque.setType(Material.BEDROCK);
+			for(int y=spawny+1;y<128;y++)source.getBlock(X, y,z1).setType(Material.AIR);
+		}
+    }
+    
+    private void limpiarChunkAtZ(Chunk source,int spawny,int Z){
+    	for(int z1=0;z1<16;z1++){
+			//result[(x1 * 16 + z1) * 128 + spawny]=(byte)Material.BEDROCK.getId();
+			Block bloque=source.getBlock(z1, spawny, Z);
+			bloque.setType(Material.BEDROCK);
+			for(int y=spawny+1;y<128;y++)source.getBlock(z1, y,Z).setType(Material.AIR);
 		}
     }
     
